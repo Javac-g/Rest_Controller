575d10a6a0a0142bc69c87f4e2d73383aac41ae9
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-05 21:10:40 +0300

    3

---
+++ V2/Controller/Car.java
@@ -0,0 +1,14 @@
+package com.MAX.V2.Controller;
+
+public class Car {
+
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
