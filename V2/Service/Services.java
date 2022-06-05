cd51f02eb2ad17b283f2ce9e9b39e4576ac3ebeb
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-05 21:21:51 +0300

    7

--- V2/Service/Services.java
+++ V2/Service/Services.java
@@ -1,4 +1,15 @@
 package com.MAX.V2.Service;
 
+import com.MAX.V2.Controller.ResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Services {
+    List<ResponseDTO> datalist = new ArrayList<>();
+    public String print(String msg){
+
+        return msg;
+
+    }
 }
