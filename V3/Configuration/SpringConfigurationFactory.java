ac88199b36486662ed59d872afd8850dd80e30ac
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-08 00:14:54 +0300

    method find

--- V3/Configuration/SpringConfigurationFactory.java
+++ V3/Configuration/SpringConfigurationFactory.java
@@ -1,5 +1,25 @@
 package com.MAX.V3.Configuration;
 
+import com.MAX.V3.Controller.Controller;
+import com.MAX.V3.Services.Services;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
 public class SpringConfigurationFactory {
+
+    @Bean(name = "Service")
+    public Services servicesBean(){
+        return new Services();
+    }
+
+    @Bean(name = "Controller")
+    @Scope(value = "prototype")
+    public Controller controllerBean(){
+
+        return new Controller(servicesBean());
+    }
+
 
 }
--- V3/Controller/Controller.java
+++ V3/Controller/Controller.java
@@ -8,12 +8,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/alpha")
 public class Controller {
     private Services services;
+
+    public Controller(Services services) {
+        this.services = services;
+    }
+
     @PostMapping(value = "/a",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public ResponseDTO addData(@RequestBody RequestDTO user){
         return services.create(user);
     }
     @GetMapping(value = "/f",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public ResponseDTO findData(@RequestParam Integer id){
         return services.read(id);
     }
+    @PutMapping(value = "/u",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO updateData(@RequestParam Integer id,@RequestBody RequestDTO user){
+        return services.update(id, user);
+    }
+    @DeleteMapping(value = "/d",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String deleteData(@RequestParam Integer id){
+        return services.print("Was deleted number: ") + services.delete(id);
+    }
+
 }
