5f911b8cc4d4370c483b56d6db677e846d4b3e49
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-07 19:14:03 +0300

    List Database

--- V3/Services/Services.java
+++ V3/Services/Services.java
@@ -1,4 +1,12 @@
 package com.MAX.V3.Services;
 
+import com.MAX.V3.Controller.ResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Services {
+
+    List<ResponseDTO> database = new ArrayList<>();
+
 }
