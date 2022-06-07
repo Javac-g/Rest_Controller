618ef3ebe044eed04fdf91eccc8e3bd6c0fb6ee9
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-07 18:40:55 +0300

    Toy

--- V3/Controller/Toy.java
+++ V3/Controller/Toy.java
@@ -1,5 +1,13 @@
 package com.MAX.V3.Controller;
 
 public class Toy {
+    String value;
 
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
