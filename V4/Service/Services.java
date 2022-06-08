377cf14f0d09b9f5ddaa4000055b0a052abd0bc6
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-08 17:43:52 +0300

    m print

---
+++ V4/Service/Services.java
@@ -0,0 +1,17 @@
+package com.MAX.V4.Service;
+
+import com.MAX.V4.Controller.ResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Services {
+    List<ResponseDTO> datalist = new ArrayList<>();
+
+    public String print(String msg){
+
+        return msg;
+
+    }
+
+}
