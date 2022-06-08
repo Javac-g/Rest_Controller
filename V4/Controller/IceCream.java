8c6c31ba30fd5f2c322e39e7cfb31e8f446beb2b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-08 16:40:16 +0300

    Ice cream

---
+++ V4/Controller/IceCream.java
@@ -0,0 +1,13 @@
+package com.MAX.V4.Controller;
+
+public class IceCream {
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
